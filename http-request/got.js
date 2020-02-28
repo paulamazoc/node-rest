8f66a763b252b23d1e3367fcb49b37cc6aaf9812
Paula <paulamazoster@gmail.com>
2020-02-27 23:51:56 -0500

    Create a http request with got

---
+++ http-request/got.js
@@ -0,0 +1,10 @@
+// 06. Hacer una petición HTTP utilizando Got
+
+const got = require('got');
+
+got('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }).then(response => {
+  console.log(response.body.url);
+  console.log(response.body.explanation);
+}).catch(error => {
+  console.log(error.response.body);
+});
