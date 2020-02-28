519b9cee154c5270a38b2e8b7061aad89f95ceed
Paula <paulamazoster@gmail.com>
2020-02-27 23:52:38 -0500

    Create a http request with superAgent

---
+++ http-request/superagent.js
@@ -0,0 +1,11 @@
+// 05. Hacer una petición HTTP utilizando la librería SuperAgent
+
+const superagent = require('superagent');
+
+superagent.get('https://api.nasa.gov/planetary/apod')
+.query({ api_key: 'DEMO_KEY', date: '2017-08-02' })
+.end((err, res) => {
+  if (err) { return console.log(err); }
+  console.log(res.body.url);
+  console.log(res.body.explanation);
+});
